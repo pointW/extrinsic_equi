71208aa10d1b277f097e6d756b9f4cd8786b6441
Dian Wang <wangdian1007@gmail.com>
2022-07-14 09:41:02 -0400

    bug fix

--- agents/sac_reg.py
+++ agents/sac_reg.py
@@ -53,6 +53,23 @@ class SACReg(SAC):
         self.networks.append(self.critic.critic_1)
         self.networks.append(self.critic.critic_2)
 
+    def getSaveState(self):
+        state = {}
+        for i in range(len(self.networks)):
+            self.networks[i].to('cpu')
+            state['{}'.format(i)] = self.networks[i].state_dict()
+            self.networks[i].to(self.device)
+        for i in range(len(self.optimizers)):
+            state['{}_optimizer'.format(i)] = self.optimizers[i].state_dict()
+        for i in range(len(self.target_networks)):
+            self.target_networks[i].to('cpu')
+            state['{}_target'.format(i)] = self.target_networks[i].state_dict()
+            self.target_networks[i].to(self.device)
+        state['alpha'] = self.alpha
+        state['log_alpha'] = self.log_alpha
+        state['alpha_optimizer'] = self.alpha_optim.state_dict()
+        return state
+
     def loadTransAndRewardModel(self, path_pre):
         """
         load the saved models
