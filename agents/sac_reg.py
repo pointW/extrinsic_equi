ceaf4307f1d25b2367b86e6f5030d91cda97e955
Dian Wang <wangdian1007@gmail.com>
2022-07-18 14:40:00 -0400

    bug fix

--- agents/sac_reg.py
+++ agents/sac_reg.py
@@ -56,7 +56,7 @@ class SACReg(SAC):
         """
         networks = self.networks + [self.actor.conv, self.critic.critic_1, self.critic.critic_2]
         for i in range(len(networks)):
-            torch.save(self.networks[i].state_dict(), '{}_{}.pt'.format(path_pre, i))
+            torch.save(networks[i].state_dict(), '{}_{}.pt'.format(path_pre, i))
 
     def loadTransAndRewardModel(self, path_pre):
         """
