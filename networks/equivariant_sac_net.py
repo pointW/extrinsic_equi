73bc3ae19bbff5960cb13ae7d083c8102ab5fed3
Dian Wang <wangdian1007@gmail.com>
2022-07-21 13:03:48 -0400

    change NonEquivariantEnc to fully conv

--- networks/equivariant_sac_net.py
+++ networks/equivariant_sac_net.py
@@ -185,9 +185,14 @@ class NonEquivariantEnc(torch.nn.Module):
                 torch.nn.Conv2d(256, 512, kernel_size=3, padding=1),
                 torch.nn.ReLU(inplace=True),
 
-                torch.nn.Flatten(),
-                torch.nn.Linear(512 * 8 * 8, n_hidden * N * 2),
+                torch.nn.Conv2d(512, 512, kernel_size=3),
                 torch.nn.ReLU(inplace=True),
+                # 6x6
+                torch.nn.MaxPool2d(2),
+                # 3x3
+                torch.nn.Conv2d(512, n_hidden * N * 2, kernel_size=3),
+                torch.nn.ReLU(inplace=True),
+                # 1x1
             )
         else:
             self.conv = torch.nn.Sequential(
@@ -207,9 +212,14 @@ class NonEquivariantEnc(torch.nn.Module):
                 torch.nn.Conv2d(256, 512, kernel_size=3, padding=1),
                 torch.nn.ReLU(inplace=True),
 
-                torch.nn.Flatten(),
-                torch.nn.Linear(512 * 8 * 8, n_hidden * N * 2),
+                torch.nn.Conv2d(512, 512, kernel_size=3),
                 torch.nn.ReLU(inplace=True),
+                # 6x6
+                torch.nn.MaxPool2d(2),
+                # 3x3
+                torch.nn.Conv2d(512, n_hidden * N * 2, kernel_size=3),
+                torch.nn.ReLU(inplace=True),
+                # 1x1
             )
 
     def forward(self, x):
