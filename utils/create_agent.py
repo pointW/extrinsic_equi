c8b58f817c392b90c57aa4677381c03062565712
Dian Wang <wangdian1007@gmail.com>
2022-07-29 13:51:54 -0400

    rename equi_both_d_w_enc_ssm+equi to equi_both_d_w_enc_ssm_equi

--- utils/create_agent.py
+++ utils/create_agent.py
@@ -258,7 +258,7 @@ def createAgent(test=False):
                 elif model == 'equi_both_d_w_enc_res_fc':
                     enc_type = 'fc'
                     backbone = 'res'
-                elif model == 'equi_both_d_w_enc_ssm+equi':
+                elif model == 'equi_both_d_w_enc_ssm_equi':
                     enc_type = 'ssm+equi'
                     backbone = 'cnn'
                 else:
